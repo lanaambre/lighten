4a2ab355b78e89dbb14c31decd53a14b265cf7e6
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-07 12:39:37 +0100

    Traitement des arguments temporaire

--- modules/light-orm/LightOrm_Table.php
+++ modules/light-orm/LightOrm_Table.php
@@ -19,25 +19,30 @@ abstract class LightOrm_Table
     return $res;
   }
 
-  public function getById($id)
+  public function getById($id = null)
   {
+    if (is_integer($id))
+      $where = ['id' =>$id];
+    else
+      $where = [];
+
     $req = new LightOrm_QueryBuilder;
     $res = $req->select()
                ->from($this->table)
-               ->where([
-                 'id' => $id,
-               ])
+               ->where($where)
                ->execute()
                ->fetchAll();
     return $res;
   }
 
-  public function getBy($column, $value = null)
+  public function getBy($column = null, $value = null)
   {
     if (is_array($column))
       $where = $column;
-    else
+    else if (is_string($column) && is_string($value))
       $where = [ $column => $value ];
+    else
+      $where = [];
 
     $req = new LightOrm_QueryBuilder;
     $res = $req->select()
--- todo.md
+++ todo.md
@@ -3,3 +3,9 @@
 - Static Config ORM
 - queryBuilder
 - Supprimer $this->db dans LightOrm_Table quand le queryBuilder sera terminé
+
+
+
+
+# Exception
+Ajouter des exceptions

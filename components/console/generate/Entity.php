fe31163e4547b751eb3d003ba5179cc9623b3255
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-12 14:46:31 +0100

    Extends Generator.php for Entity

--- components/console/generate/Entity.php
+++ components/console/generate/Entity.php
@@ -4,17 +4,13 @@ namespace components\console\generate;
 
 use components\ORM\OrmConfig;
 
-class Entity
+class Entity extends Generator
 {
   private $table;
   private $structure;
 
-  private $arguments;
-
-  public function __construct($args)
+  protected function init()
   {
-    $this->arguments = $args;
-
     if (empty($this->arguments[0]))
       exit("Missing 1 parameters. <name> parameter is require for: generate:entity <name> <table(?)>\n");
 

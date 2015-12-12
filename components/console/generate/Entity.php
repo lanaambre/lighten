84e3bc025cc9f920562581c9b876548f2639d399
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-13 00:26:35 +0100

    Add PHP Generator code

--- components/console/generate/Entity.php
+++ components/console/generate/Entity.php
@@ -4,18 +4,41 @@ namespace components\console\generate;
 
 use components\ORM\OrmConfig;
 
-class Entity extends Generator
+class Entity extends PhpGenerator
 {
+  private $name;
   private $table;
   private $structure;
 
-  protected function init()
+  protected function config()
   {
     if (empty($this->arguments[0]))
       exit("Missing 1 parameters. <name> parameter is require for: generate:entity <name> <table(?)>\n");
 
+    $this->name = $this->arguments[0];
     $this->table = !empty($this->arguments[1]) ? $this->arguments[1] : $this->generateTableName($this->arguments[0]);
     $this->structure = $this->getStructure();
+
+    $this->namespace = 'src\Entity';
+    $this->use = [
+      'components\ORM\OrmTable',
+    ];
+
+    $this->code();
+  }
+
+  private function code()
+  {
+    $this->initCode();
+    $this->addNamespace();
+    $this->addUse();
+
+    var_dump($this->name);
+    $this->addClass($this->name, 'OrmTable'); // Open Class
+    $this->addComments('Todo');
+    $this->closeClass(); // Close Class
+
+    file_put_contents("test.php", $this->code);
   }
 
   private function generateTableName($entity)
---
+++ components/console/generate/PhpGenerator.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace components\console\generate;
+
+class PhpGenerator
+{
+  protected $arguments;
+  protected $code = '';
+  protected $namespace = '';
+  protected $use = '';
+  protected $tabs = 0;
+  protected $tabsLength = 2;
+
+  public function __construct($args)
+  {
+    $this->arguments = $args;
+    $this->config();
+  }
+
+  protected function initCode()
+  {
+    $this->addCode('<?php');
+    $this->addLineBreak(2);
+  }
+
+  protected function addNamespace()
+  {
+    if (!empty($this->namespace)) {
+      $this->addCode('namespace ' . $this->namespace . ';');
+      $this->addLineBreak(2);
+    }
+  }
+
+  protected function addUse()
+  {
+    if (!empty($this->use)) {
+      foreach ($this->use as $value) {
+        $this->addCode('use ' . $value . ';');
+      }
+      $this->addLineBreak(2);
+    }
+  }
+
+  protected function addCode($code)
+  {
+    $this->code .= $this->doTabs() . $code;
+  }
+
+  protected function addLineBreak($numb = 1)
+  {
+    for ($i=0; $i < $numb; $i++) {
+      $this->code .= "\n";
+    }
+  }
+
+  protected function doTabs()
+  {
+    for ($i=0; $i < $this->tabs; $i++) {
+      for ($i=0; $i < $this->tabsLength; $i++) {
+        $this->code .= ' ';
+      }
+    }
+  }
+
+  protected function addTabs($numb = 1)
+  {
+    $this->tabs += $numb;
+  }
+
+  protected function removeTabs($numb = 1)
+  {
+    for ($i=0; $i < $numb; $i++) {
+      if ($this->tabs > 0)
+        $this->tabs = $this->tabs - 1;
+    }
+  }
+
+  protected function addClass($name, $extends = '', $implements = '')
+  {
+    $this->addCode('class ' . $name);
+
+    if (!empty($extends))
+      $this->addCode(' extends ' . $extends);
+    if (!empty($implement))
+      $this->addCode(' implements' . $implements);
+
+    $this->addLineBreak();
+
+    $this->addCode('{');
+    $this->addLineBreak();
+    $this->addTabs();
+  }
+
+  protected function closeClass()
+  {
+    $this->addLineBreak();
+    var_dump($this->tabs);
+    $this->removeTabs();
+    var_dump($this->tabs);
+    $this->addCode('}');
+  }
+
+  protected function addComments($comments)
+  {
+    $this->addCode('// ' . $comments);
+  }
+}

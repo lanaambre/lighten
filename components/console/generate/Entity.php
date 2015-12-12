34486a8f4dc3553ebf90b9f1b83789f49b3a6519
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-12 14:38:40 +0100

    getStructure of table: OK

--- components/console/generate/Entity.php
+++ components/console/generate/Entity.php
@@ -7,6 +7,8 @@ use components\ORM\OrmConfig;
 class Entity
 {
   private $table;
+  private $structure;
+
   private $arguments;
 
   public function __construct($args)
@@ -17,12 +19,16 @@ class Entity
       exit("Missing 1 parameters. <name> parameter is require for: generate:entity <name> <table(?)>\n");
 
     $this->table = empty($this->arguments[1]) ? $this->generateTableName($this->arguments[0]) : $this->arguments[1];
+
+    $this->structure = $this->getStructure();
+
+    var_dump($this->structure);
   }
 
   private function generateTableName($entity)
   {
     $table = strtolower($entity);
-    
+
     if (substr($table, -1) == 'y')
       $table = rtrim($table, 'y') . 'ies';
     else
@@ -33,10 +39,13 @@ class Entity
 
   private function getStructure()
   {
+    $db = OrmConfig::getConnexion();
 
-    // $db = OrmConfig::getConnexion();
-    //
-    // $query = $db->query('SHOW COLUMNS FROM users');
-    // var_dump($query->fetchAll(PDO::FETCH_COLUMN));
+    $query = $db->query('SHOW COLUMNS FROM ' . $this->table);
+
+    if ($query)
+      return $query->fetchAll(\PDO::FETCH_COLUMN);
+    else
+      exit("Table '" . $this->table . "' doesn't exists\n");
   }
 }

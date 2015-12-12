3d2f7a77fb32f2aef720522573d1eb50fce77342
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-12 13:52:00 +0100

    Add generateTableName() into generate/Entity.php

--- components/console/generate/Entity.php
+++ components/console/generate/Entity.php
@@ -2,9 +2,8 @@
 
 namespace components\console\generate;
 
-/**
- *
- */
+use components\ORM\OrmConfig;
+
 class Entity
 {
   private $table;
@@ -13,7 +12,23 @@ class Entity
   public function __construct($args)
   {
     $this->arguments = $args;
-    var_dump($args);
+
+    if (empty($this->arguments[0]))
+      exit("Missing 1 parameters. <name> parameter is require for: generate:entity <name> <table(?)>\n");
+
+    $this->table = empty($this->arguments[1]) ? $this->generateTableName($this->arguments[0]) : $this->arguments[1];
+  }
+
+  private function generateTableName($entity)
+  {
+    $table = strtolower($entity);
+    
+    if (substr($table, -1) == 'y')
+      $table = rtrim($table, 'y') . 'ies';
+    else
+      $table .= 's';
+
+    return $table;
   }
 
   private function getStructure()

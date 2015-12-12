ee438954863aad130dadf2e0ff67725a05555fb1
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-12 22:39:06 +0100

    Update generateTable

--- components/console/generate/Entity.php
+++ components/console/generate/Entity.php
@@ -14,23 +14,13 @@ class Entity extends Generator
     if (empty($this->arguments[0]))
       exit("Missing 1 parameters. <name> parameter is require for: generate:entity <name> <table(?)>\n");
 
-    $this->table = empty($this->arguments[1]) ? $this->generateTableName($this->arguments[0]) : $this->arguments[1];
-
+    $this->table = !empty($this->arguments[1]) ? $this->arguments[1] : $this->generateTableName($this->arguments[0]);
     $this->structure = $this->getStructure();
-
-    var_dump($this->structure);
   }
 
   private function generateTableName($entity)
   {
-    $table = strtolower($entity);
-
-    if (substr($table, -1) == 'y')
-      $table = rtrim($table, 'y') . 'ies';
-    else
-      $table .= 's';
-
-    return $table;
+    return strtolower(substr($entity, -1) == 'y' ? rtrim($entity, 'y') . 'ies' : $entity . 's');
   }
 
   private function getStructure()

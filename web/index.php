78db1c4ccd0cd9f21e61f629e8211714dfa1a28e
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-15 15:35:30 +0100

    Add new config in index.php

--- web/index.php
+++ web/index.php
@@ -7,8 +7,9 @@ use components\ORM\OrmConfig;
 use src\Controller\UserController;
 use components\tools\Yaml\Yaml;
 
-$db_infos = Yaml::parse(file_get_contents('../config/db.yml'));
+$config = Yaml::parse(file_get_contents('../config/config.yml'));
+$database = $config['database'];
 
-OrmConfig::init($db_infos);
+OrmConfig::init($database);
 
 $home = new UserController;

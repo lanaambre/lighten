190680009e78c7e931cf84f1e0acaffbbb8305a0
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-15 15:16:55 +0100

    Config generator ok

--- components/console/project/Init.php
+++ components/console/project/Init.php
@@ -8,46 +8,74 @@ use components\tools\Yaml\Yaml;
 class Init
 {
   private $input = [];
+  private $directoryPath;
 
   public function __construct()
   {
+    $this->directoryPath = getcwd();
+
     echo "-------------------\n";
     echo "New Lighten project\n";
     echo "-------------------\n";
 
-    echo "Project Name: ";
+
+    // Project
+    echo "Project Name [" . $this->getDirectory() . "]: ";
 
     $stdin = fopen('php://stdin', 'r');
     $response = fgets($stdin); // use fgetc for get only first character
-    $this->input['project'] = rtrim($response, "\n");
+    $response = rtrim($response, "\n");
+    $response = empty($response) ? $this->getDirectory() : $response;
+    $this->input['project'] = $response;
 
-    echo "Database host: ";
 
-    $stdin = fopen('php://stdin', 'r');
-    $response = fgets($stdin);
-    $this->input['database']['db_host'] = rtrim($response, "\n");
-
-    echo "Database Name: ";
+    // Host
+    echo "Database Host [localhost]: ";
 
     $stdin = fopen('php://stdin', 'r');
     $response = fgets($stdin);
-    $this->input['database']['db_name'] = rtrim($response, "\n");
+    $response = rtrim($response, "\n");
+    $response = empty($response) ? 'localhost' : $response;
+    $this->input['database']['db_host'] = $response;
 
-    echo "Database Username: ";
 
-    $stdin = fopen('php://stdin', 'r');
-    $response = fgets($stdin);
-    $this->input['database']['db_user'] = rtrim($response, "\n");
-
-    echo "Database Password: ";
+    // Name
+    echo "Database Name [" . strtolower($this->getDirectory()) . "]: ";
 
     $stdin = fopen('php://stdin', 'r');
     $response = fgets($stdin);
-    $this->input['database']['db_password'] = rtrim($response, "\n");
+    $response = rtrim($response, "\n");
+    $response = empty($response) ? strtolower($this->getDirectory()) : $response;
+    $this->input['database']['db_name'] = $response;
 
-    echo "Config ok ?\n";
-    var_dump($this->input);
 
-    file_put_contents('test.yml', Yaml::dump($this->input, 2));
+    // User
+    echo "Database Username [root]: ";
+
+    $stdin = fopen('php://stdin', 'r');
+    $response = fgets($stdin);
+    $response = rtrim($response, "\n");
+    $response = empty($response) ? 'root' : $response;
+    $this->input['database']['db_user'] = $response;
+
+
+    // Password
+    echo "Database Password [root]: ";
+
+    $stdin = fopen('php://stdin', 'r');
+    $response = fgets($stdin);
+    $response = rtrim($response, "\n");
+    $response = empty($response) ? 'root' : $response;
+    $this->input['database']['db_password'] = $response;
+
+    echo "Config ok: config/config.yml has been generated.\n";
+
+    file_put_contents('config/config.yml', Yaml::dump($this->input, 2));
+  }
+
+  private function getDirectory()
+  {
+    $splitted = explode('/', $this->directoryPath);
+    return array_pop($splitted);
   }
 }
